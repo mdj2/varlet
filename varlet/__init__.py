a4a363da41f92cde29ae5b039e1ac7a0c4a23f0f
Matt Johnson <mdj2@pdx.edu>
2014-11-17 11:52:22 -0800

    We need to reload the variable module every call to variable() since it gets cached by Python

--- varlet/__init__.py
+++ varlet/__init__.py
@@ -23,6 +23,7 @@ location of __main__
 from __future__ import print_function
 import os
 import sys
+import ast
 import inspect
 import tokenize
 import token
@@ -30,12 +31,27 @@ import readline
 import importlib
 from functools import partial
 from collections import defaultdict
+import logging
 # these are imported so I can use mock easily to patch them. Also note that I
 # alias raw_input to input for Python2
 try:
     from builtins import input, print
 except ImportError:
     from __builtin__ import raw_input as input, print
+
+# try to get reload in Py3 to Py3.3
+try:
+    from imp import reload
+except ImportError:
+    pass
+
+# try to get reload in Py 3.4
+try:
+    from importlib import reload
+except ImportError:
+    pass
+
+logger = logging.getLogger(__name__)
 
 class AnsiFormatCode:
     BLACK = 90
@@ -70,27 +86,37 @@ A_UNIQUE_VALUE = float("nan")
 # the name of the module in the python path that we should load variables from
 VARIABLES_MODULE = "variables"
 
-# a dictionary of configuration options
-variables = {}
-try:
-    variables_module = importlib.import_module(VARIABLES_MODULE)
-    # the path to the VARIABLES_MODULE
-    VARIABLES_PATH = os.path.abspath(os.path.join(variables_module.__file__))
-    variables = dict((k, v) for k, v in variables_module.__dict__.items() if not k.startswith("__"))
-except ImportError as e:
-    # the variables module doesn't exist in the PYTHONPATH, so we will create
-    # it in the same location as __main__
-    VARIABLES_PATH = os.path.abspath(os.path.join(os.path.dirname(getattr(sys.modules["__main__"], '__file__', '')), VARIABLES_MODULE + ".py"))
-    # the default location isn't in sys.path, so error out
-    if os.path.dirname(VARIABLES_PATH) not in sys.path:
-        raise RuntimeError("You need to create a %s.py file somewhere in your PYTHONPATH!" % (VARIABLES_MODULE))
+def load_variables():
+    """
+    Returns a 2-tuple where the first element is a dict of variables defined in
+    the variables module, and the second element is the path (or expected path) to
+    the module
+    """
+    try:
+        variables = importlib.import_module(VARIABLES_MODULE)
+        # reload the module to see if anything was changed since we originally
+        # loaded it. For some reason, Python caches the module and doesn't
+        # reload it automatically when it changes
+        reload(variables)
+        # the path to the VARIABLES_MODULE
+        variables_path = os.path.abspath(os.path.join(variables.__file__))
+        variables = dict((k, v) for k, v in variables.__dict__.items() if not k.startswith("__"))
+    except (OSError, ImportError) as e:
+        variables = {}
+        # the variables module doesn't exist in the PYTHONPATH, so we will create
+        # it in the same location as __main__
+        variables_path = os.path.abspath(os.path.join(os.path.dirname(getattr(sys.modules["__main__"], '__file__', '')), VARIABLES_MODULE + ".py"))
+        # the default location isn't in sys.path, so error out
+        if os.path.dirname(variables_path) not in sys.path:
+            raise RuntimeError("You need to create a %s.py file somewhere in your PYTHONPATH!" % (VARIABLES_MODULE))
 
-    print(success("A %s.py file will be created in %s after you set a variable" % (VARIABLES_MODULE, VARIABLES_PATH)))
+        logging.info("A %s.py file will be created in %s after you set a variable" % (VARIABLES_MODULE, variables_path))
+
+    return variables, variables_path
 
 
 def variable(name, default=A_UNIQUE_VALUE):
-    global variables
-
+    variables, variables_path = load_variables()
     # if the name of the variable is not defined, we need to prompt the user for it
     if name not in variables:
         has_default = default is not A_UNIQUE_VALUE
@@ -106,7 +132,7 @@ def variable(name, default=A_UNIQUE_VALUE):
         while name not in variables:
             # make sure we are at a tty device
             if not os.isatty(sys.stdin.fileno()):
-                raise KeyError("You need to set the variable '%s' in %s (or somewhere else in your python path)." % (name, VARIABLES_PATH))
+                raise KeyError("You need to set the variable '%s' in %s (or somewhere else in your python path)." % (name, variables_path))
 
             val = input(warning(name) + " = ")
             # clear the startup hook since we only want to show the default
@@ -114,7 +140,7 @@ def variable(name, default=A_UNIQUE_VALUE):
             readline.set_startup_hook()
 
             if has_default and val == "":
-                variables[name] = default
+                val = default
             else:
                 try:
                     val = eval(val)
@@ -122,18 +148,18 @@ def variable(name, default=A_UNIQUE_VALUE):
                     print(danger(str(e)))
                     continue
 
-                # we need to ensure the repr of the value is valid Python
-                try:
-                    eval(repr(val))
-                except Exception as e:
-                    print(danger("The value must have a repr that is valid Python (like a number, list, dict, or string)!"))
-                    continue
+            # we need to ensure the repr of the value is valid Python
+            try:
+                ast.literal_eval(repr(val))
+            except Exception as e:
+                print(danger("The value must have a repr that is valid Python (like a number, list, dict, or string)!"))
+                continue
 
-                # everything is good, so we can actually save the value
-                variables[name] = val
+            # everything is good, so we can actually save the value
+            variables[name] = val
 
         # append the variable to the variables.py file
-        with open(VARIABLES_PATH, "a+") as f:
+        with open(variables_path, "a+") as f:
             # write a newline if we're not at the beginning of the file, and
             # the previous line didn't end with \n
             write_new_line = f.tell() != 0 and not f.read().endswith("\n")
